257c303c54dfbacefbed39d0a2272f161baec372
Madhav Pandey <madpande@adobe.com>
2023-02-24 15:06:41 -0700

    Initial commit

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,6 +1,7 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
+  debugger
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
